a545e359a678a168412285901ebcdb52988a8b3f
syashwant19 <95519358+syashwant19@users.noreply.github.com>
2022-02-10 14:47:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ import smtplib
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
-engine.setProperty('voice', voices[1].id)
-
+engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
@@ -23,12 +21,12 @@ def wishMe():
         speak("Good Morning!")
 
     elif hour>=12 and hour<18:
-        speak("Good Afternoon!")
+        speak("Good Afternoon!")   
 
     else:
-        speak("Good Evening!")
+        speak("Good Evening!")  
 
-    speak("I am  Sir. Please tell me how may I help you")
+    speak("I am Shri Sir. Please tell me how may I help you")       
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
@@ -40,13 +38,13 @@ def takeCommand():
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")
+        print("Recognizing...")    
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
 
     except Exception as e:
-        # print(e)
-        print("Say that again please...")
+        # print(e)    
+        print("Say that again please...")  
         return "None"
     return query
 
@@ -54,7 +52,7 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
+    server.login('youremail@gmail.com', 'password')
     server.sendmail('youremail@gmail.com', to, content)
     server.close()
 
@@ -80,30 +78,30 @@ if __name__ == "__main__":
             webbrowser.open("google.com")
 
         elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")
-
+            webbrowser.open("stackoverflow.com")   
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'D:\\MP3'
             songs = os.listdir(music_dir)
-            print(songs)
+            print(songs)    
             os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'email ' in query:
+        elif 'email to Shree' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"
+                to = "Shree@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend . I am not able to send this email")
+                speak("Sorry ! I am not able to send this email")    
+
